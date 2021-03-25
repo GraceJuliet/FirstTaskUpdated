e0a2587524de984242e2c1d6aaf4d483c69f4e83
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-03-25 11:29:50 +0530

    Add files via upload

---
+++ FirstTaskUpdated/FirstTaskUpdated/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace FirstTaskUpdated
+{
+   public class Employee
+
+    {
+     /* public Employee()
+        {
+          Firstname = "Maneesh";
+          Lastname = "Dani";
+          Dateofbirth = "23-02-1998";
+          Experience = "2";
+        }*/
+
+        private string firstname;
+        public string Firstname
+        {
+            get; set;
+        }
+        private string lastname;
+        public string Lastname
+        {
+            get; set;
+        }
+
+
+
+        private string dateofbirth;
+        public string Dateofbirth
+        {
+            get; set;
+        }
+
+
+
+        private string experience;
+        public string Experience
+        {
+            get; set;
+        }
+
+      
+    }
+}
